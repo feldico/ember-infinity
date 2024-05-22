f6d16cbaa7804c204fc087baca1502d8f1bcb408
marcemira <hello@marcemira.com>
2024-05-22 14:50:13 -0300

    Adding current dist

---
+++ ember-infinity/dist/lib/infinity-model.js
@@ -0,0 +1,4 @@
+import '@ember/array/proxy';
+export { I as default } from '../infinity-model-DxqGNvIf.js';
+import 'rsvp';
+//# sourceMappingURL=infinity-model.js.map
---
+++ ember-infinity/dist/utils.js
@@ -0,0 +1,3 @@
+import '@ember/object';
+export { c as checkInstanceOf, a as convertToArray, o as objectAssign, p as paramsCheck } from './infinity-model-DxqGNvIf.js';
+//# sourceMappingURL=utils.js.map
